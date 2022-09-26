4837eaced800beda4efabf7db7241036ce651966
Sravani Sandhya Devi Pyla <sandhyapyla.8@gmail.com>
2022-09-26 06:33:23 +0530

    sometimes map function is not working for forecast hours

--- src/components/ForeCast.js
+++ src/components/ForeCast.js
@@ -27,61 +27,61 @@ const progress={
   width:"70%"
 }
 
-  return (
-    <div style={forecastSection}>
-      forecast for {city}
-     {
-      forecastday.map((curDateForecast)=>{
-        const {date,day,hour}=curDateForecast;
-        console.log(date,day,hour)
-        const {maxtemp_c,mintemp_c,daily_chance_of_rain,condition:{text}}=day;
-        console.log(maxtemp_c,mintemp_c,daily_chance_of_rain)
-        return (
-          // console.log(date,day,hour)
-          <Accordion expanded={expanded === date} onChange={handleChange(date)}>
-          <AccordionSummary
-            expandIcon={<ExpandMoreIcon />}
-            aria-controls="panel2bh-content"
-            id={date}
-          >
-            {/* <img src={icon}/> */}
-            <Typography sx={{ width: '33%', flexShrink: 0 }}>{date}
-            ({text})
-            </Typography>
-            <Typography sx={{ width: '33%', flexShrink: 0 }}>
-              <b>Temp:</b>{mintemp_c} deg to {maxtemp_c} deg
-            </Typography>
-            <Typography sx={{ width: '33%', flexShrink: 0 }}>
-              <b>{daily_chance_of_rain}</b> % of rain possible
-            </Typography>
+  // return (
+  //   <div style={forecastSection}>
+  //     forecast for {city}
+  //    {
+  //     forecastday.map((curDateForecast)=>{
+  //       const {date,day,hour}=curDateForecast;
+  //       console.log(date,day,hour)
+  //       const {maxtemp_c,mintemp_c,daily_chance_of_rain,condition:{text}}=day;
+  //       console.log(maxtemp_c,mintemp_c,daily_chance_of_rain)
+  //       return (
+  //         // console.log(date,day,hour)
+  //         <Accordion expanded={expanded === date} onChange={handleChange(date)}>
+  //         <AccordionSummary
+  //           expandIcon={<ExpandMoreIcon />}
+  //           aria-controls="panel2bh-content"
+  //           id={date}
+  //         >
+  //           {/* <img src={icon}/> */}
+  //           <Typography sx={{ width: '33%', flexShrink: 0 }}>{date}
+  //           ({text})
+  //           </Typography>
+  //           <Typography sx={{ width: '33%', flexShrink: 0 }}>
+  //             <b>Temp:</b>{mintemp_c} deg to {maxtemp_c} deg
+  //           </Typography>
+  //           <Typography sx={{ width: '33%', flexShrink: 0 }}>
+  //             <b>{daily_chance_of_rain}</b> % of rain possible
+  //           </Typography>
            
-          </AccordionSummary>
-          <AccordionDetails>
-            {
-              hour.map((curHourForCast,index)=>{
-                return (
-                  <div style={hourtrack}>
-                    <b>{index}:00</b> 
-                    {/* <img src={curHourForCast.condition.icon}/> */}
-                    <div style={progress}>
-                    <LinearProgress variant="determinate" value={(curHourForCast.temp_c*100/maxtemp_c)} />
-                    {curHourForCast.temp_c} deg
-                    </div>
-                    {/* {curHourForCast.chance_of_rain} */}
-                  </div>
-                )
-              })
-            }
-          </AccordionDetails>
+  //         </AccordionSummary>
+  //         <AccordionDetails>
+  //           {
+  //             hour.map((curHourForCast,index)=>{
+  //               return (
+  //                 <div style={hourtrack}>
+  //                   <b>{index}:00</b> 
+  //                   {/* <img src={curHourForCast.condition.icon}/> */}
+  //                   <div style={progress}>
+  //                   <LinearProgress variant="determinate" value={(curHourForCast.temp_c*100/maxtemp_c)} />
+  //                   {curHourForCast.temp_c} deg
+  //                   </div>
+  //                   {/* {curHourForCast.chance_of_rain} */}
+  //                 </div>
+  //               )
+  //             })
+  //           }
+  //         </AccordionDetails>
 
-        </Accordion>
-        ) 
+  //       </Accordion>
+  //       ) 
        
-      })
-     }
+  //     })
+  //    }
      
-    </div>
-  )
+  //   </div>
+  // )
 }
 
 export default ForeCast
